4299ab0bf27d59bf5b6c30997c45330ad5c2ec2c
huascar321 <oxhuascar321@gmail.com>
2022-04-05 19:35:39 -0400

    messages exporting correctly

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './Input.scss';
 
-function Input() {
+function Input({ socket, messages, setMessages }) {
+  const [text, setText] = React.useState("");
+
+  const sendData = () => {
+    socket.emit("message", {
+      text: text
+    });
+    let temp = messages;
+    temp.push({
+      user: "client",
+      text: text
+    });
+    setMessages([...temp])
+  };
   return(
     <div className="submit-container">
-      <textarea className="submit-container__textarea" placeholder="Escribe tu mensaje..."/>
-      <button className="submit-container__submit" type="submit">Enviar</button>
+      <input 
+        value={text} 
+        onChange={(e) => setText(e.target.value)} 
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            sendData();
+          }
+        }} 
+        className="submit-container__textarea" placeholder="Escribe tu mensaje..."/>
+      <button className="submit-container__submit" onClick={sendData}>Enviar</button>
     </div>
   );
 }
